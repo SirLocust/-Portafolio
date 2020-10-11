eda36d821f87738adc646906a96923662188d326
sirlocust <jua56yu@hotmail.com>
2020-10-11 16:47:54 -0500

    se hace retoques al navBar y se crea la logica para el componente section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Portafolio';
+export class AppComponent implements OnInit {
+  title = '';
+  textWelcome = 'Bienvenido a mi sitio Web...';
+
+  ngOnInit(): void {
+    this.typeEffect(this.textWelcome);
+  }
+
+  typeEffect(txt: string): void {
+    let count = 0;
+    const audio = new Audio('/assets/typewriter.mp3');
+    audio.load();
+    const intervalo = setInterval(() => {
+      
+      // console.log(this.title.concat('s'));
+      audio.play()
+      this.title += txt.charAt(count);
+
+      count++;
+      if (count >= txt.length) {
+        audio.pause();
+        clearInterval(intervalo);
+      }
+    }, 100);
+  }
 }
