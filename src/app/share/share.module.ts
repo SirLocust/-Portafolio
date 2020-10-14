37bcb07e591ede5d71e27133a70381fc18498f78
sirlocust <jua56yu@hotmail.com>
2020-10-14 10:26:01 -0500

    se crea el componente cards se agrega un mixin global de textshadow

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -4,19 +4,23 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { NavButtonComponent } from './components/nav-button/nav-button.component';
 import { SectionComponent } from './section/section.component';
 import { ButtonScrollToComponent } from './button-scroll-to/button-scroll-to.component';
-
-
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
-  declarations: [NavbarComponent, NavButtonComponent, SectionComponent, ButtonScrollToComponent],
-  imports: [
-    CommonModule,
+  declarations: [
+    NavbarComponent,
+    NavButtonComponent,
+    SectionComponent,
+    ButtonScrollToComponent,
+    CardsComponent,
   ],
+  imports: [CommonModule],
   exports: [
     NavbarComponent,
     NavButtonComponent,
     SectionComponent,
-    ButtonScrollToComponent
-  ]
+    ButtonScrollToComponent,
+    CardsComponent,
+  ],
 })
-export class ShareModule { }
+export class ShareModule {}
