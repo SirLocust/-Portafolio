77e03de8afb4bd04febe2d4f7dc71a974496dc92
sirlocust <jua56yu@hotmail.com>
2020-10-21 19:07:20 -0500

    se quita el background negro al container component

--- src/app/skills/components/containerskill/containerskill.component.ts
+++ src/app/skills/components/containerskill/containerskill.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-containerskill',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./containerskill.component.scss']
 })
 export class ContainerskillComponent implements OnInit {
-
+  @HostBinding('class.flex_center') flexCenter = true;
+  @Input()  logoUrl: string = '/';
+  isOnLogo: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleIsOnLogo(): void{
+    this.isOnLogo = !this.isOnLogo;
   }
 
 }
