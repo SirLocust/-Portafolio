8ccb8b83cc59b954b61d399af02bcf799411ed96
sirlocust <jua56yu@hotmail.com>
2020-10-29 00:21:24 -0500

    se crea la logica para detectar el value del screen de la ventana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  isLandscape = false;
+  texto = 's';
 
+  constructor(private render2: Renderer2) {}
 
   ngOnInit(): void {
-
+    this.isLandScape();
+    this.render2.listen('window', 'orientationchange', (event) => {
+      this.isLandScape();
+    });
   }
-
-
+  change() {
+    console.log('s');
+    document.documentElement.requestFullscreen();
+    screen.orientation.lock('landscape-primary');
+  }
+  isLandScape(): void {
+    // console.log('s');
+    this.texto = window.screen.orientation.type;
+    console.log(window.screen.orientation.type);
+    if (window.screen.orientation.type !== 'landscape-primary') {
+      this.isLandscape = false;
+    }
+    if (window.screen.orientation.type === 'landscape-primary') {
+      this.isLandscape = true;
+    }
+  }
 }
