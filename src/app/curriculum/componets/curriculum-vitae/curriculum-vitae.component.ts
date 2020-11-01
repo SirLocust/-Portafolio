6a80b851960aa74e8b7b6ce6466bbb2048b4fe88
unknown <jua56yu@hotmail.com>
2020-11-01 13:41:56 -0500

    Se pasa de animaciones css a animación con angular animatios

--- src/app/curriculum/componets/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/curriculum/componets/curriculum-vitae/curriculum-vitae.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations'
 
 @Component({
   selector: 'app-curriculum-vitae',
   templateUrl: './curriculum-vitae.component.html',
-  styleUrls: ['./curriculum-vitae.component.scss']
+  styleUrls: ['./curriculum-vitae.component.scss'],
+  animations:[
+    trigger('toggleViewCv', [
+      state('off', style({
+        width : '0'
+      })),
+      state('on', style({
+        width: '62.5em'
+      })),
+      transition('off => on', animate('700ms ease-in-out')),
+      transition('on => off', animate('700ms ease-in-out')),
+
+    ])
+  ]
 })
 export class CurriculumVitaeComponent implements OnInit {
   isViewCv = false;
   isViewCv1 = false;
-
+  animationViewCv = 'on'
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  toggleCv(){
+  toggleCv(): void {
     this.isViewCv = !this.isViewCv;
+    // setTimeout(() => {
+    this.animationViewCv = (this.animationViewCv === 'off') ? 'on' : 'off'
+
+    // }, 100);
   }
 
   
--- src/app/curriculum/curriculum.module.ts
+++ src/app/curriculum/curriculum.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CurriculumRoutingModule } from './curriculum-routing.module';
 import { CurriculumVitaeComponent } from './componets/curriculum-vitae/curriculum-vitae.component';
 import { SliderCvComponent } from './componets/slider-cv/slider-cv.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [CurriculumVitaeComponent, SliderCvComponent],
   imports: [
     CommonModule,
-    CurriculumRoutingModule
+    CurriculumRoutingModule,
+    // BrowserAnimationsModule
   ]
 })
 export class CurriculumModule { }
