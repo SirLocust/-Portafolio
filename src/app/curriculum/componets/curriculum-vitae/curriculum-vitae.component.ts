28561b8e761c791dc7ecb3793baa8b6cea7b2554
unknown <jua56yu@hotmail.com>
2020-11-01 15:15:45 -0500

    lógica del componente curriculum terminada se organiza la lógica de slider-cv  para utilizarlo como componente reutilizable

--- src/app/curriculum/componets/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/curriculum/componets/curriculum-vitae/curriculum-vitae.component.ts
@@ -5,41 +5,38 @@ import { trigger, state, style, transition, animate} from '@angular/animations'
   selector: 'app-curriculum-vitae',
   templateUrl: './curriculum-vitae.component.html',
   styleUrls: ['./curriculum-vitae.component.scss'],
-  animations:[
-    trigger('toggleViewCv', [
-      state('off', style({
-        width : '0'
-      })),
-      state('on', style({
-        width: '62.5em'
-      })),
-      transition('off => on', animate('700ms ease-in-out')),
-      transition('on => off', animate('700ms ease-in-out')),
-
-    ])
-  ]
+  
 })
 export class CurriculumVitaeComponent implements OnInit {
-  isViewCv = false;
-  isViewCv1 = false;
-  animationViewCv = 'on'
-
+  isViewCvAnimationList = [
+    {
+      isViewCvAnimation : 'off'
+    } ,
+    {
+      isViewCvAnimation : 'off'
+    } ,
+    {
+      isViewCvAnimation : 'on'
+    } 
+    
+  ]
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
 
   toggleCv(): void {
-    this.isViewCv = !this.isViewCv;
+   console.log(this.isViewCvAnimationList)
     // setTimeout(() => {
-    this.animationViewCv = (this.animationViewCv === 'off') ? 'on' : 'off'
+   
 
     // }, 100);
   }
 
   
   toggleCv1(){
-    this.isViewCv1 = !this.isViewCv1;
+   
   }
 
 }
