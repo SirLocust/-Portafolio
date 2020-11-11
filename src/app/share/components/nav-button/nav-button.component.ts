0b8b1628210ad4b363fccf6b3afa9e27b09cc302
sirlocust <jua56yu@hotmail.com>
2020-11-10 21:16:41 -0500

    se le asigna las rutas al menu de navegación principal se le da un color para todos los items

--- src/app/share/components/nav-button/nav-button.component.ts
+++ src/app/share/components/nav-button/nav-button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NavButtonComponent implements OnInit {
   @Input() textoAnchor = '';
   @Input() colorBoxShadow = 'red';
+  @Input() target = '/';
   constructor() { }
 
   ngOnInit(): void {
