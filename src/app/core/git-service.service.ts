8ff418c13eba61469cb5f0e3bd109c6e1592e85f
sirlocust <jua56yu@hotmail.com>
2020-11-14 15:04:11 -0500

    se deja las bases de la logica del componente portafolio

--- src/app/core/git-service.service.ts
+++ src/app/core/git-service.service.ts
@@ -11,12 +11,10 @@ export class GitServiceService {
   user = 'SirLocust';
   headerAuthorization = new HttpHeaders({
     Accept: 'application/vnd.github.v3.html',
-    Authorization: 'token 3dfa11968aa53ed60f71cd10b1112d4ce6ad9d0c'
+    // Authorization: 'token auttoke'
   });
   constructor(private http: HttpClient) {
-  //   this.http.get('https://github.com/login/oauth/authorize?client_id=1c4ebdb1f4114c9fa30c').subscribe( data => {
-  //     console.log(data)
-  //   })
+
   }
 
   getAllRespositories(): Observable<any> {
--- src/app/portafolio/portafolio.module.ts
+++ src/app/portafolio/portafolio.module.ts
@@ -6,10 +6,11 @@ import { PortafolioBodyComponent } from './components/portafolio-body/portafolio
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { RealProyectsComponent } from './components/real-proyects/real-proyects.component';
 import { CardGitComponent } from './components/card-git/card-git.component';
+import { ExerciseCourseComponent } from './components/exercise-course/exercise-course.component';
 
 
 @NgModule({
-  declarations: [PortafolioBodyComponent, SideBarComponent, RealProyectsComponent, CardGitComponent],
+  declarations: [PortafolioBodyComponent, SideBarComponent, RealProyectsComponent, CardGitComponent, ExerciseCourseComponent],
   imports: [
     CommonModule,
     PortafolioRoutingModule
