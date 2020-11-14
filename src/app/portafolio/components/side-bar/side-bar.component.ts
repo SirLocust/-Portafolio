5464d6092f2ee9c426549b80cb85cba2d92ee79b
sirlocust <jua56yu@hotmail.com>
2020-11-13 20:20:41 -0500

    se crea los diseños y estilos para el futuro componente cardGit

--- src/app/portafolio/components/side-bar/side-bar.component.ts
+++ src/app/portafolio/components/side-bar/side-bar.component.ts
@@ -12,6 +12,7 @@ export class SideBarComponent implements OnInit {
 
   ngOnInit(): void {
     this.gitservice.getUser().subscribe( (user) => {
+      console.log(user)
       this.user = user;
     })
   }
