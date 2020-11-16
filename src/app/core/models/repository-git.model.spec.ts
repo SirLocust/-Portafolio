0507846d60c429f7f81b57269560637546e80fa6
sirlocust <jua56yu@hotmail.com>
2020-11-16 01:19:02 -0500

    se crean las interfaces de los datos que entrega la api gitHub y se le asigna los tipos de datos a  los métodos del servicio que conecta con esta api

---
+++ src/app/core/models/repository-git.model.spec.ts
@@ -0,0 +1,7 @@
+import { RepositoryGit } from './repository-git.model';
+
+describe('RepositoryGit', () => {
+  it('should create an instance', () => {
+    expect(new RepositoryGit()).toBeTruthy();
+  });
+});
--- src/app/portafolio/components/real-proyects/real-proyects.component.ts
+++ src/app/portafolio/components/real-proyects/real-proyects.component.ts
@@ -1,27 +1,29 @@
+import { RepositoryGit } from './../../../core/models/repository-git.model';
 import { GitServiceService } from './../../../core/git-service.service';
 import { Component, OnInit } from '@angular/core';
-import { filter, map} from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 @Component({
   selector: 'app-real-proyects',
   templateUrl: './real-proyects.component.html',
-  styleUrls: ['./real-proyects.component.scss']
+  styleUrls: ['./real-proyects.component.scss'],
 })
 export class RealProyectsComponent implements OnInit {
-  repos = [];
-  constructor(public gitService: GitServiceService) { }
+  repos: RepositoryGit[] = [];
+  constructor(public gitService: GitServiceService) {}
 
   ngOnInit(): void {
-    this.gitService.getAllRespositories()
-    .pipe(
-      map( (data) => {
-        return  data.filter( data => {
-           return data.name.charAt(0) === '-';
-           })
-      })
-    )
-    .subscribe( (repos) => {
-      this.repos = repos;
-    });
+    this.gitService
+      .getAllRespositories()
+      .pipe(
+        map((repositories: RepositoryGit[]) => {
+          console.log(repositories);
+          return repositories.filter((repository: RepositoryGit) => {
+            return repository.name.charAt(0) === '-';
+          });
+        })
+      )
+      .subscribe((repositories: RepositoryGit[]) => {
+        this.repos = repositories;
+      });
   }
-
 }
